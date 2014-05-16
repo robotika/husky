1b75578f88a37e8d6a07fc230bebd597d4c3bbdf
martind <martind@mapfactor.com>
2014-05-16 18:46:40 +0200

    hacked ver0 for Robotem Rovne 2014

--- rr.py
+++ rr.py
@@ -16,12 +16,16 @@ horizon.open()
 def status_handler(code, payload, timestamp):
   print(payload.print_format())
 
-horizon.add_handler(status_handler, request = 'system_status')
-horizon.request_system_status(subscription = 1)
+#horizon.add_handler(status_handler, request = 'system_status')
+#horizon.request_system_status(subscription = 1)
 
-for i in range(1, 6):
+for i in range(1, 3):
   left_percent = right_percent = i * 10
   horizon.set_differential_output(left_percent, right_percent)
+  time.sleep(0.5)
+
+while True:
+  horizon.set_differential_output(30, 30)
   time.sleep(0.5)
 
 time.sleep(1)
