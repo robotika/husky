009e69483e5b11c3c11e750ad14c4e455a7ea15b
martind <martind@mapfactor.com>
2014-08-24 15:36:32 +0200

    hacked /hello to log some /imu/data

--- ros/hello.py
+++ ros/hello.py
@@ -8,7 +8,8 @@
 # terminalA:   roscore
 # terminalB:   rostopic pub /hello std_msgs/String "Hello Robot"
 
-ROS_MASTER_URI = 'http://192.168.33.105:11311'
+#ROS_MASTER_URI = 'http://192.168.33.105:11311'
+ROS_MASTER_URI = 'http://192.168.1.11:11311'
 
 MY_CLIENT_URI = 'http://192.168.33.100:8000'
 
@@ -39,27 +40,33 @@ for s in systemState[0]:
   print s
 
 caller_id = "/hello_test_client"
-topic = "/hello"
-topic_type = "std_msgs/String"
+#topic = "/hello"
+topic = "/imu/data"
+#topic_type = "std_msgs/String"
+topic_type = "std_msgs/Imu"
 caller_api = MY_CLIENT_URI # for "publisherUpdate"
 
 code, statusMessage, topicTypes = master.getTopicTypes(caller_id)
 assert code == 1, code
 print "---"
 print "topicTypes:"
-print topicTypes
+for t in topicTypes:
+    print t
 print "---"
 
 code, statusMessage, publishers = master.registerSubscriber(caller_id, topic, topic_type, caller_api)
 assert code == 1, code
 assert len(publishers) == 1, publishers # i.e. fails if publisher is not ready now
 
+print publishers
 publisher = ServerProxy( publishers[0].replace("martind-ThinkPad-R60", "192.168.33.105") ) # "pseudo DNS"
 code, statusMessage, protocolParams = publisher.requestTopic( caller_id, topic, [["TCPROS"]] )
 assert code == 1, code
 assert len(protocolParams) == 3, protocolParams
 
-hostPortPair = ( "192.168.33.105", protocolParams[2] )
+print protocolParams
+#hostPortPair = ( "192.168.33.105", protocolParams[2] )
+hostPortPair = ( protocolParams[1], protocolParams[2] )
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP
 
 
@@ -71,22 +78,27 @@ soc.settimeout( 3.0 )
 
 header = prefix4BytesLen(
         prefix4BytesLen( "callerid=/hello_test_client" ) +
-        prefix4BytesLen( "topic=/hello" ) +
-        prefix4BytesLen( "type=std_msgs/String" ) +
-        prefix4BytesLen( "md5sum=992ce8a1687cec8c8bd883ec73ca41d1" ) +
+        prefix4BytesLen( "topic="+topic ) +
+        prefix4BytesLen( "type="+topic_type ) +
+#        prefix4BytesLen( "md5sum=992ce8a1687cec8c8bd883ec73ca41d1" ) +
+        prefix4BytesLen( "md5sum=6a62c6daae103f4ff57a132d6f95cec2" ) +
         "" )
 
 
 soc.send( header )
-while 1:
+f = open("tmp.bin","wb")
+for i in xrange(10):
     print "--------------------"
     data = soc.recv(4)
+    f.write( data )
     size = struct.unpack("I", data)[0]
     print "SIZE", size
     data = soc.recv( size )
+    f.write( data )
+    f.flush()
     for s in splitLenStr(data):
         print s
-
+f.close()
 
 #-------------------------------------------------------------------
 # vim: expandtab sw=4 ts=4
